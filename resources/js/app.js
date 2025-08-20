5ab1d9379d91431fcd52a9e979cdf629d5ff24ec
waseem abbas <waseemofficee@gamil.com>
2025-08-20 21:22:37 +0500

    add user ID meta tag and implement chat message listener

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@
  */
 
 import './echo';
+const userId = document.querySelector('meta[name="user-id"]')?.getAttribute('content'); // or however you get user ID
+
+if (userId) {
+    window.Echo.private(`chat.${userId}`)
+        .listen('MessageSent', (data) => {
+            console.log('New Chat Message Notification:', data);
+        });
+}
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,4 +1,7 @@
 <x-layouts.app.sidebar :title="$title ?? null">
+    @auth
+    <meta name="user-id" content="{{ auth()->id() }}">
+@endauth
     <flux:main>
         {{ $slot }}
     </flux:main>
