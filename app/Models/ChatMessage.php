c5363acd918cb44798abf91bfdf65b2019341c56
waseem abbas <waseemofficee@gamil.com>
2025-08-20 17:44:49 +0500

    create chat and install reverb

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatMessage extends Model
 {
-    //
+    protected $fillable = [ "id", "sender_id","receiver_id","message"];
+
+    public function sender(){
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+    public function receiver(){
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
 }
