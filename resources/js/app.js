058202cdb85e5b81587b6c018cc7b2660bbad58b
waseem abbas <waseemofficee@gamil.com>
2025-08-22 11:49:47 +0500

    build code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
-
 import './echo';
 const userId = document.querySelector('meta[name="user-id"]')?.getAttribute('content'); // or however you get user ID
 
